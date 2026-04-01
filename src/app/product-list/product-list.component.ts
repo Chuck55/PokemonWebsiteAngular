77ddbcc3829c2382b2474eaafc1dd7baa76601a3
Chuck55 <41636138+Chuck55@users.noreply.github.com>
2026-04-01 02:28:25 -0500

    adding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -29,15 +29,15 @@ export class ProductListComponent {
   informElec() {
     window.alert("This pokemon is shocking");
   }
-  /*
+  
   showIt(id) {
-    $('#imageshow').on('', this.showIt);
+    //$('#imageshow').on('', this.showIt);
     var Url = document.getElementById('imageshow') as HTMLInputElement;
     console.log(id);
     Url.src=id;
     Url.style.display='block';
   }
-*/
+
 }
 /*
 Copyright Google LLC. All Rights Reserved.
